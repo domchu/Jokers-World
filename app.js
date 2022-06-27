b310e2472bbb4098ac5b0c21de2f26ef0653de23
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-06-27 23:07:02 +0100

    added the url/api

---
+++ app.js
@@ -0,0 +1,9 @@
+const urlApi =
+  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+const category = document.querySelector(".category");
+const jokeBtn = document.querySelector("joke-btn");
+const jokeSetUp = document.querySelector(".joke-setup");
+const jokeDelivery = document.querySelector(".joke-delivery");
+const loader = document.querySelector(".loader");
+
+jokeBtn.addEventListener("click", () => {});
