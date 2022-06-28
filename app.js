55e36d273a09d9f86957cd5cbb4ef44374e7e87b
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-06-28 02:24:34 +0100

    added the safe status

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 // const api = "https://v2.jokeapi.dev/joke/Any?safe-mode";
-const url =
+const urlApi =
   "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 const category = document.querySelector(".category");
 const jokeBtn = document.querySelector(".joke-btn");
 const jokeSetUp = document.querySelector(".joke-setup");
 const jokeDelivery = document.querySelector(".joke-delivery");
-const loader = document.querySelector(".loader");
+const loaderGif = document.querySelector(".loader");
+const jokeStatus = document.querySelector(".joke-status");
 
 jokeBtn.addEventListener("click", () => {
-  loader.style.display = "block";
-  fetch(url)
+  loaderGif.style.display = "block";
+  fetch(urlApi) //fatch the data
     .then((res) => {
-      return res.json();
+      return res.json(); //convert the res to json
     })
     .then((data) => {
-      //   console.log(data);
-      loader.style.display = "none";
+      //the converted data
+      console.log(data);
+      loaderGif.style.display = "none";
       if (data.type == "single") {
         jokeDelivery.textContent = data.joke;
         category.textContent = data.category;
+        jokeStatus.textContent = data.safe;
         // jokeSetUp.textContent = "";
       } else {
         category.textContent = data.category;
